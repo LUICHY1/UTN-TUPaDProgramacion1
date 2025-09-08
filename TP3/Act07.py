390476e7ba83d24af3741239ecb01d3aaf9415bf
LUICHY1 <luisalbertocastrollanos@gmail.com>
2025-09-08 11:35:34 -0300

    Actividad 7

---
+++ TP3/Act07.py
@@ -0,0 +1,23 @@
+#Escribe un programa que permita al usuario ingresar 100 números enteros. Luego, el programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad menor, pero debe estar preparado para procesar 100 números con un solo cambio).
+
+pares = 0
+impares = 0
+positivos = 0
+negativos = 0
+cantidad = 100
+print(f"Ingresá {cantidad} números enteros:")
+for i in range(cantidad):
+    numero = int(input(f"Número {i+1}: "))
+    if numero % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+    if numero > 0:
+        positivos += 1
+    elif numero < 0:
+        negativos += 1
+print("\nResultados:")
+print("Cantidad de números pares:", pares)
+print("Cantidad de números impares:", impares)
+print("Cantidad de números positivos:", positivos)
+print("Cantidad de números negativos:", negativos)
