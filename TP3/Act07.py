bb1d5c51d6b46069bc2031c526248b2b49e8ce83
LUICHY1 <luisalbertocastrollanos@gmail.com>
2025-09-16 18:00:57 -0300

    Update Act07.py

--- TP3/Act07.py
+++ TP3/Act07.py
@@ -1,23 +1,8 @@
-#Escribe un programa que permita al usuario ingresar 100 números enteros. Luego, el programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad menor, pero debe estar preparado para procesar 100 números con un solo cambio).
-
-pares = 0
-impares = 0
-positivos = 0
-negativos = 0
-cantidad = 100
-print(f"Ingresá {cantidad} números enteros:")
-for i in range(cantidad):
-    numero = int(input(f"Número {i+1}: "))
-    if numero % 2 == 0:
-        pares += 1
-    else:
-        impares += 1
-    if numero > 0:
-        positivos += 1
-    elif numero < 0:
-        negativos += 1
-print("\nResultados:")
-print("Cantidad de números pares:", pares)
-print("Cantidad de números impares:", impares)
-print("Cantidad de números positivos:", positivos)
-print("Cantidad de números negativos:", negativos)
+#Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un número entero positivo indicado por el usuario
+if numero >= 0:
+    suma = 0
+    for i in range(numero + 1): 
+        suma += i
+    print(f"La suma de los números entre 0 y {numero} es: {suma}")
+else:
+    print("Por favor, ingresa un número entero positivo.")
